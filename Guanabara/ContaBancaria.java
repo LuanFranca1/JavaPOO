fb49a2ae8b37a201a2de21b1d46377cf4a39ea23
LuanFranca1 <99998698+LuanFranca1@users.noreply.github.com>
2022-04-26 12:39:13 -0300

    Update ContaBancaria.java

--- Guanabara/ContaBancaria.java
+++ Guanabara/ContaBancaria.java
@@ -1,107 +1,144 @@
-package aula05;
+package javaapplication2;
 
-public class Secundaria {
+public class Conta {
 
     public int numConta;
-    private char abrirConta;
+    protected String tipoConta;
     private String donoConta;
-    private double saldo = 0;
-    private boolean status = falso;
-    public Secundaria(int numConta, String abrirConta, String donoConta, double saldo, boolean status) {
+    private double saldo;
+    private boolean status;
+
+    //métodos personalizados;
+    public void estadoAtual() {
+        System.out.println("Conta " + this.getNumConta());
+        System.out.println("TIpo conta" + this.getTipoConta());
+        System.out.println("Status da conta " + this.getStatus());
+        System.out.println("O saldo da conta " + this.getSaldo());
+        System.out.println("O dono da conta " + this.getDonoConta());
+    }
+
+    public void abrirConta(String t) {
+        this.setTipoConta(t);
+        this.setStatus(true);
+        if (t == "CC ") {
+            this.setSaldo(50);
+        } else if (t == "CP") {
+            this.setSaldo(150);
+        }
+        System.out.println("Conta aberta com sucesso!");
+    }
+
+    public void fecharConta() {
+        this.setStatus(true);
+        if (this.getSaldo() > 0) {
+            //Não pode fechar a conta por ainda ter dinheiro;
+        } else if (this.getSaldo() <= 0) {
+            //não pode fechar a conta pois o saldo está negativo;
+        } else {
+            this.setStatus(false);
+            //A conta será fechada com êxito;
+        }
+    }
+
+    public void depositar(double v) {
+
+        if (getStatus()) {
+            //this.saldo = this.saldo + v;
+            this.setSaldo(this.getSaldo() + v);
+            //Deposito realizado com sucesso!
+        } else {
+            //impossível depositar se a conta não está aberta;
+        }
+    }
+
+    public void sacar(double saque) {
+        if (getStatus()) {
+            if (this.saldo > saque) {
+                //this.saldo = this.saldo - saque;
+                this.setSaldo(this.getSaldo() - saque);
+                System.out.println("Saque efetuado com sucesso!");
+            } else {
+                //Não tem todo esse dinheiro na conta;
+            }
+        } else {
+            //A conta está fechada;
+        }
+    }
+
+    public void pagarMensalidade() {
+        int v = 0;
+        if (this.getTipoConta() == "CC") {
+            v = 12;
+        } else if (this.getTipoConta() == "CP") {
+            v = 20;
+        }
+        if (this.getStatus()) {
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Mensalidade paga com sucesso!" + this.getDonoConta());
+        } else {
+            //Não dá para pagar a conta se ela n estiver aberta;
+        }
+    }
+
+    public Conta(String donoConta,int numConta,boolean status, String tipoConta,double saldo) {
         this.numConta = numConta;
-        this.abrirConta = abrirConta;
+        this.tipoConta = tipoConta;
+        this.setSaldo(0);
+        this.setStatus(false);
+
         this.donoConta = donoConta;
-        this.saldo = saldo;
-        this.status = status;
     }
-    public void setAbrirConta(char abrirConta) {
-        this.abrirConta = abrirConta;
-    }
+
     public int getNumConta() {
         return numConta;
     }
+
     public void setNumConta(int numConta) {
         this.numConta = numConta;
     }
-    public boolean isStatus() {
-        return status;
+
+    public void setStatus(boolean s) {
+        this.status = s;
     }
-    public void setStatus(boolean status) {
-        this.status = status;
+
+    public boolean getStatus() {
+        return this.status;
     }
-    public String getDonoConta() {
-        return donoConta;
+
+    public void setTipoConta(String tipoConta) {
+        this.tipoConta = tipoConta;
     }
-    public void setDonoConta(String donoConta) {
-        this.donoConta = donoConta;
+
+    public String getTipoConta() {
+        return this.tipoConta;
     }
-    public double getSaldo() {
-        return saldo;
-    }
+
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-    public char getAbrirConta() {
-        this.setAbrirConta(t);
-        this.setStatus(true);
-        if (t == "CC") {
-            saldo = 50; //setsaldo(50);
-        } else if (t == "CP") {
-            saldo = 150;
-        }
-        return this.abrirConta;
-    }
-    public char getFecharConta() {
-        if (saldo > 0) {
-            //Aconta ainda tem saldo;
-        } else if (saldo < 0) {
-            //não pode fechar a conta
-        } else if (saldo == 0) {
-            //está apto a fechar a conta;
-        }
-    }
-    public boolean depositar(double dinheiro) {
-        if (status == true) {
-            setSaldo(getSaldo() + dinheiro);
-        } else {
-            //IMpossível depositar se a conta n estiver aberta
-        }
+
+    public double getSaldo() {
         return this.saldo;
     }
-    public double saldo(double dinheiro) {
-        if (stauts == true) {
-            if (saldo > dinheiro) {
-                saldo = saldo - dinheiro;
-            }
-        } else {
-            //não tem como tirar dinheir se essa porra for falsa;
-        }
-        return this.saldo;
-    }
-    public double PagarMensalidade(char t) {
-        double valor;
-        if (t == "CC") {
-            valor = 12;
-        } else if(t="CP") {
-            valor = 20;
-        }
-        if(status=true) {
-            if(saldo>valor){
-                saldo = saldo - valor;
-        }else if(saldo <= 0) {
-            }else  {
-                //impossível pagar se a conta n está aberta;
-            }
-            }
-        return ;
-    }
-}
-package aula05;
 
-public class Principal {
-    public static void main(String[]args){
-        Secundaria c1 = new Secundaria();
-        c1.status = true;
+    public String getDonoConta() {
+        return donoConta;
     }
+
+    public void setDonoConta(String donoConta) {
+        this.donoConta = donoConta;
+    }
+
 }
 
+package javaapplication2;
+
+public class JavaApplication2 {
+    public static void main(String[] args) {
+        Conta p1 = new Conta();
+        p1.setDonoConta("jubilosca apenas");
+        p1.setNumConta(101010);
+        p1.setStatus(true);
+    }
+    
+}
